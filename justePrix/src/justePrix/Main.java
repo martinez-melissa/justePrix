8c00f1b10f555a9f99bb89365389ce12875a0385
Melissa Martinez <melissa.martinez@divcom.ch>
2022-02-15 17:46:41 +0100

    fin du codage

---
+++ justePrix/src/justePrix/Main.java
@@ -0,0 +1,48 @@
+package justePrix;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// déclaration et initialisation des variables
+		Random r = new Random(); 
+		//int justeprix = (int) Math.random()*200;
+		int justeprix = r.nextInt(200);
+		int essai = 0;
+
+		// affichage console
+		System.out.println("Entrez un numéro entre 0 et 200:");
+
+		// entrée console
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		int prix = scan.nextInt();
+
+		// boucle de saisie et affichage du juste prix tant qu'il n'est pas trouvé
+		while(justeprix != prix) {
+
+			// boucle +/-
+			if (prix < justeprix && prix > 0) {
+				System.out.println("C'est plus");
+			} 
+			else if (prix > justeprix && prix < 200) {
+				System.out.println("c'est moins");
+			} 
+			else {
+				System.out.println("Veuillez choisir entre 0 et 200");
+			}
+
+			// nouvelle entrée console
+			prix = scan.nextInt();
+			essai++;
+
+		}
+
+		// bonne réponse
+		System.out.println("Bravo, vous avez trouvé le juste prix en " + essai + " essais");
+
+	}
+
+}
